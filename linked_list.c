64b1a567712cc76e9ea0a375a63949185049297b
Jagrav-M <jagrav.muddasani@gmail.com>
2025-04-09 18:10:46 -0400

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,7 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) 
-{ 
-  struct list_node *node = malloc(sizeof(struct list_node));
-  if(!node)
-  {
-    return NULL;
-  }
-  node->value = value;
-  node->next = NULL;
-  return node;
-}
+struct list_node *new_node(size_t value) {return NULL; }
 
 void insert_at_head(struct linked_list *list, size_t value) 
 {
